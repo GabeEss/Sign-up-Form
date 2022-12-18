6cf30ab077cb6cb17e271b3b55969900d5684ab4
Gabe S <gabesliwowicz@gmail.com>
2022-12-18 17:07:10 -0500

    Styled the right side bar, styled the main part of the page. Added screen reader support. Added requirements for email and password.

--- myscript.js
+++ myscript.js
@@ -2,5 +2,8 @@ document.getElementById('login-link')
 .addEventListener('click', function(event) {
     event.preventDefault(); // prevent navigating to URL
     document.getElementById('login-form').style.display
-    = 'block'; // display the login form as a block
+    = 'flex'; // display the login form as flex
+    document.getElementById('login-form').style.flexDirection = 'row';
+    document.getElementById('login-form').style.marginBottom = '10px';
+    document.getElementById('login-form').style.gap = '5px';
 })
