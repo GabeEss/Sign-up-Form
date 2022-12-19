4445b0483832ac6e5a638183b6bd17b4283fef11
Gabe S <gabesliwowicz@gmail.com>
2022-12-18 19:09:23 -0500

    Added scripting to ensure password and confirm-password met requirements. Set up phone number to only except 10 digits, if that input had been filled out.

--- myscript.js
+++ myscript.js
@@ -1,3 +1,4 @@
+// If the login-link element is clicked, it enables the login form and styles it.
 document.getElementById('login-link')
 .addEventListener('click', function(event) {
     event.preventDefault(); // prevent navigating to URL
@@ -6,4 +7,37 @@ document.getElementById('login-link')
     document.getElementById('login-form').style.flexDirection = 'row';
     document.getElementById('login-form').style.marginBottom = '10px';
     document.getElementById('login-form').style.gap = '5px';
-})
+});
+
+const passwordInput = document.getElementById('password');
+const confirmInput = document.getElementById('confirm-password');
+const requirementsDiv = document.getElementById('password-requirements');
+const confirmDiv = document.getElementById('confirm-requirements');
+
+// Test password input. If valid, the inner text changes.
+passwordInput.addEventListener('input', () => {
+    if (passwordInput.value.length >= 8
+    && isValidPassword(passwordInput.value)) {
+    requirementsDiv.innerText = 'Password meets requirements';
+    } else {
+    requirementsDiv.innerText = 'Password must be at least 8 characters long and contain one capital letter and one number';
+    }
+});
+
+
+// Check if the password contains at least one capital letter and one number
+function isValidPassword(password) {
+    const hasCapitalLetter = /[A-Z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    
+    return hasCapitalLetter && hasNumber;
+    }
+
+// Test confirm password.
+confirmInput.addEventListener('input', () => {
+    if(passwordInput.value == confirmInput.value)
+        confirmDiv.innerText = 'Password confirmed.';
+        else
+            confirmDiv.innerText = 'Confirm Password must be the same as Password.';
+});
+
